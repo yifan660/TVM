535eac8bc1f93c19de3622c7976d508f92de86a7
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-01-12 19:26:16 +0800

    Create vision_layers.cc

---
+++ vision_layers.cc
@@ -0,0 +1,29 @@
+Expr MakePixelShuffle() {
+
+}
+
+TVM_REGISTER_GLOBAL();
+
+Expr MakePixelUnshuffle()   {
+
+}
+
+TVM_REGISTER_GLOBAL();
+
+Expr MakeUpsample() {
+
+}
+
+TVM_REGISTER_GLOBAL();
+
+Expr MakeUpsamplingBilinear2d() {
+
+}
+
+TVM_REGISTER_GLOBAL();
+
+Expr MakeUpsamplingNearest2d()  {
+    
+}
+
+TVM_REGISTER_GLOBAL();
