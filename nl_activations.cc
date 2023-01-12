563c6d78f87b1b45e3564745ad9c1195a8d5cd0f
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-01-12 19:27:28 +0800

    Create nl_activations.cc

---
+++ nl_activations.cc
@@ -0,0 +1,104 @@
+Expr MakeELU()   {
+    Attrs<Integer> 
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeHardshrink()  {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeHardsigmoid() {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeHardtanh()  {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeHardswish()  {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeLeakyReLU()   {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeLogSigmoid()  {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeMultiheadAttention()    {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakePReLU()  {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeReLU()    {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeReLU6()    {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeRReLU()   {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeSELU() {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeHuberLoss()    {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeSmoothL1Loss() {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeSoftMarginLoss()   {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeMultiLabelSoftMarginLoss() {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeCosineEmbeddingLoss()  {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeMultiMarginLoss()  {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeTripletMarginLoss()    {
+
+}
+TVM_REGISTER_GLOBAL();
+
+Expr MakeTripletMarginWithDistanceLoss()    {
+
+}
+TVM_REGISTER_GLOBAL();
