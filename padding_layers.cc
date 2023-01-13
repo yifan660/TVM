618d68568d0c571d41f78123ef3bb4e3c8d2f405
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-01-13 11:03:06 +0800

    Update padding_layers.cc

--- padding_layers.cc
+++ padding_layers.cc
@@ -1,39 +1,49 @@
 Expr MakeReflectionPad1d()  {
 
 }
+TVM_REGISTER_GLOBAL();
 
 Expr MakeReflectionPad2d()  {
 
 }
+TVM_REGISTER_GLOBAL();
 
 Expr MakeReflectionPad3d()  {
 
 }
+TVM_REGISTER_GLOBAL();
 
 Expr MakeReplicationPad1d() {
 
 }
+TVM_REGISTER_GLOBAL();
 
 Expr MakeReplicationPad2d() {
 
 }
+TVM_REGISTER_GLOBAL();
 
 Expr MakeReplicationPad3d() {
 
 }
+TVM_REGISTER_GLOBAL();
 
 Expr MakeZeroPad2d()    {
 
 }
+TVM_REGISTER_GLOBAL();
 
 Expr MakeConstantPad1d()    {
 
 }
+TVM_REGISTER_GLOBAL();
 
 Expr MakeConstantPad2d()    {
 
 }
+TVM_REGISTER_GLOBAL();
 
 Expr MakeConstantPad3d()    {
     
 }
+TVM_REGISTER_GLOBAL();
