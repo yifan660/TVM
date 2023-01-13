ebbbc184633d9c541b511ed7a58aadc9945888b0
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-01-13 11:31:18 +0800

    Update pooling_layers.cc

--- pooling_layers.cc
+++ pooling_layers.cc
@@ -1,55 +1,94 @@
 Expr MakeMaxPool1d() {
 
 }
+TVM_REGISTER_GLOBAL(MaxPool1d);
 
 Expr MakeMaxPool2d() {
     
 }
+TVM_REGISTER_GLOBAL(MaxPool2d);
 
 Expr MakeMaxPool3d() {
     
 }
+TVM_REGISTER_GLOBAL(MaxPool3d);
 
 Expr MakeMaxUnpool1d() {
     
 }
+TVM_REGISTER_GLOBAL(MaxUnpool1d);
 
 Expr MakeMaxUnpool2d() {
     
 }
+TVM_REGISTER_GLOBAL(MaxUnpool2d);
 
 Expr MakeMaxUnpool3d()   {
 
 }
+TVM_REGISTER_GLOBAL(MaxUnpool3d);
 
 Expr MakeAvgPool1d() {
 
 }
+TVM_REGISTER_GLOBAL(AvgPool1d);
 
 Expr MakeAvgPool2d() {
 
 }
+TVM_REGISTER_GLOBAL(AvgPool2d);
 
 Expr MakeAvgPool3d() {
 
 }
+TVM_REGISTER_GLOBAL(AvgPool3d);
 
 Expr MakeFractionalMaxPool2d() {
 
 }
+TVM_REGISTER_GLOBAL(FractionalMaxPool2d);
 
 Expr MakeFractionalMaxPool3d() {
 
 }
+TVM_REGISTER_GLOBAL(FractionalMaxPool3d);
 
 Expr MakeLPPool1d() {
 
 }
+TVM_REGISTER_GLOBAL(LPPool1d);
 
 Expr MakeLPPool2d() {
 
 }
+TVM_REGISTER_GLOBAL(LPPool2d);
 
-Expr MakeLPPool1d() {
+Expr MakeAdaptiveMaxPool1d()    {
 
 }
+TVM_REGISTER_GLOBAL(AdaptiveMaxPool1d);
+
+Expr MakeAdaptiveMaxPool2d()    {
+
+}
+TVM_REGISTER_GLOBAL(AdaptiveMaxPool2d);
+
+Expr MakeAdaptiveMaxPool3d()    {
+
+}
+TVM_REGISTER_GLOBAL(AdaptiveMaxPool3d);
+
+Expr MakeAdaptiveAvgPool1d()    {
+
+}
+TVM_REGISTER_GLOBAL(ReflectionPad1d);
+
+Expr MakeAdaptiveAvgPool2d()    {
+    
+}
+TVM_REGISTER_GLOBAL(AdaptiveAvgPool2d);
+
+Expr MakeAdaptiveAvgPool3d()    {
+    
+}
+TVM_REGISTER_GLOBAL(AdaptiveAvgPool3d);
