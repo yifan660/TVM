472cb7039e2a7e640527c3a55e6dff3c1c0eb341
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-01-13 11:17:34 +0800

    Create pooling_layers.cc

---
+++ pooling_layers.cc
@@ -0,0 +1,27 @@
+Expr MakeMaxPool1d() {
+
+}
+
+Expr MakeMaxPool2d() {
+    
+}
+
+Expr MakeMaxPool3d() {
+    
+}
+
+Expr MakeMaxUnpool1d() {
+    
+}
+
+Expr MakeMaxUnpool2d() {
+    
+}
+
+Expr MakeMaxUnpool3d()   {
+
+}
+
+Expr MakeAvgPool1d() {
+
+}
