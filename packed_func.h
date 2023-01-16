4b486d7d056b7477691f34624e184cc8ce7de762
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-01-17 00:00:59 +0800

    Create packed_func.h

--- packed_func.h
+++ packed_func.h
@@ -21,4 +21,18 @@ class TVMRetValue : public TVMPODValue_ {
                 value_.v_handle = new T(v);
             }
         }
+
+        using TVMPODValue_::operator double;
+        using TVMPODValue_::operator int64_t;
+        using TVMPODValue_::operator uint64_t;
+        using TVMPODValue_::operator int;
+        using TVMPODValue_::operator bool;
+        using TVMPODValue_::operator void*;
+        using TVMPODValue_::operator DLTensor*;
+        using TVMPODValue_::operator Device;
+        using TVMPODValue_::operator NDArray;
+        using TVMPODValue_::operator Module;
+        using TVMPODValue_::operator PackedFunc;
+        
 }
+
