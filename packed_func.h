df6b70a656b4ea9e76dc73827da63e8fd7d6486a
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-01-17 16:15:10 +0800

    Update packed_func.h

--- packed_func.h
+++ packed_func.h
@@ -5,26 +5,11 @@ class TVMRetValue : public TVMPODValue_ {
             other.value_.v_handle = nullptr;
             other.type_code_ = kTVMNullptr;
         }
-        ~TVMRetValue()
-
-        void SwitchToPOD()  {
-            if(type_code_!=type_code)   {
-                this->Clear();
-                type_code_ = type_code;
-            }
-        }
-        template<typename T>
-        void SwitchToClass(int type_code, T v)  {
-            if(type_code_!=type_code)   {
-                this->Clear();
-                type_code_ = type_code;
-                value_.v_handle = new T(v);
-            }
-        }
-
+        ~TVMRetValue()  {this->Clear();}
+        
         using TVMPODValue_::operator double;
         using TVMPODValue_::operator int64_t;
-        using TVMPODValue_::operator uint64_t;
+        using TVMPODValue_::operator uint6_t;
         using TVMPODValue_::operator int;
         using TVMPODValue_::operator bool;
         using TVMPODValue_::operator void*;
@@ -33,6 +18,60 @@ class TVMRetValue : public TVMPODValue_ {
         using TVMPODValue_::operator NDArray;
         using TVMPODValue_::operator Module;
         using TVMPODValue_::operator PackedFunc;
-        
+        using TVMPODValue_::AsObjectRef;
+        using TVMPODValue_::IsObjectRef;
+
+        TVMRetvalue(const TVMRetValue& other) : TVMPODValue_()  {this->Assign(other);}
+        operator std::string() const    {
+            if(type_code_)
+        }
+
+        void SwitchToPOD(int type_code)  {
+            if(type_code_!=type_code)   {
+                this->Clear();
+                type_code_ = type_code;
+            }
+        }
+
+        template<typename T>
+        void SwitchToClass(int type_code, T v)    {
+            if(type_code_!=type_code)    {
+                this->Clear();
+                type_code_ = type_code;
+                value_.v_handle = new T(v);
+            } else  {
+                *static_cast<T*>(value_.v_handle) = v;
+            }
+        }
+
+        void SwitchToObject(int type_code, ObjectPtr<Object> other)    {
+            if(other.data_!=nullptr)    {
+                this->Clear();
+                type_code_ = type_code;
+                value_.v_handle = other.data_;
+                other.data_ = nullptr;
+            } else  {
+                SwitchToPOD(kTVMNullptr);
+                value_.v_handle = nullptr;
+            }
+        }
+
+        void Clear()    {
+            if()    {
+                
+            }
+            switch()    {
+
+            }
+        }
 }
+
+// IsObjectRef will be called only when ObjectRef is base class of TObjectRef
+template<typename TObjectRef, typename = typename std::enable_if<std::is_base_of<ObjectRef, TObjectRef>::value>::type>
+inline bool IsObjectRef() const;
+
+template<typename
+
+
+
 
