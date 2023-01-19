47a9b83af13f37abbd425668274044321d023216
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-01-19 17:30:48 +0800

    Update flatten_buffer.cc

--- flatten_buffer.cc
+++ flatten_buffer.cc
@@ -12,6 +12,20 @@ class BufferFlattener : public StmtExprMutator  {
     
     private:
         explicit BufferFlattener(const Map<Var, Buffer>& extern_buffer_map) {
-            
+            for(const auto& kv : extern_buffer_map)   {
+                updated_extern_buffer_map_.Set(kv.first, GetFlattenedBuffer(kv.second));
+            }
         }
+
+        Stmt VisitStmt_(const BlockNode* op) final  {
+            ICHECK_EQ(op->match_buffer.size(),0) << "Unexpected MatchBufferRegion found during tir.transform.FlattenBuffer." << "All MatchBufferRegion should be removed in tir.transform.LowerMatchBuffer.";
+            Block block = GetRef<Block>(op);
+            Array<BufferRegion> reads = op->reads;
+            reads.MutateByApply()
+        }
+}
+
+class Allocate : public Stmt    {
+    public:
+        TVM_DLL Allocate(Var )
 }
