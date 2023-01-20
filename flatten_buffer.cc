ce6b0423d101dded2c297c67422440e31dcfcd33
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-01-20 16:14:28 +0800

    Update flatten_buffer.cc

--- flatten_buffer.cc
+++ flatten_buffer.cc
@@ -20,8 +20,28 @@ class BufferFlattener : public StmtExprMutator  {
         Stmt VisitStmt_(const BlockNode* op) final  {
             ICHECK_EQ(op->match_buffer.size(),0) << "Unexpected MatchBufferRegion found during tir.transform.FlattenBuffer." << "All MatchBufferRegion should be removed in tir.transform.LowerMatchBuffer.";
             Block block = GetRef<Block>(op);
+            Array<BufferRegion> alloc_buffers = op->alloc_buffers;
+            alloc_buffers.MutateByApply([this](Buffer buf)  { return GetFlattenedBuffer(buf); });
+            if(!alloc_buffers.same_as(op->alloc_buffers))   {
+                block.CopyOnWrite()->alloc_buffers = alloc_buffers;
+            }
+
             Array<BufferRegion> reads = op->reads;
-            reads.MutateByApply()
+            reads.MutateByApply([this](BufferRegion region) { return MutateBufferRegion(region); });
+            if(!reads.same_as(op->reads))   {
+                block.CopyOnWrite()->reads = reads;
+            }
+
+            Array<BufferRegion> writes = op->writes;
+            writes.MutateByApply([this](BufferRegion region) { return MutateBufferRegion(region); });
+            if(!writes.same_as(op->writes))   {
+                block.CopyOnWrite()->writes = writes;
+            }
+        }
+
+        Stmt VisitStmt_(const AllocateNode* op) final {
+            Allocate alloc = Downcast<Allocate>(StmtExprMutator::VisitStmt_(op));
+            if(alloc->dtype == DataType::Bool())
         }
 }
 
@@ -39,5 +59,16 @@ class AllocateConstNode : public StmtNode   {
         Array<PrimExpr> extents;
         Stmt body;
 
+        void VisitAttrs(AttrVisitor* v)   {
+            v->Visit("buffer_var", &);
+            v->Visit();
+            v->Visit();
+            v->Visit();
+            v->Visit();
+            v->Visit();
+            v->Visit();
+            v->Visit();
+        }
 };
+
 
